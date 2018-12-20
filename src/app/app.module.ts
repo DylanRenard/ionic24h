a616eb1f0539c5619f997edf7ddf089624a38a8e
Unknown <dylan.renard.72@gmail.com>
2018-12-20 09:21:56 +0100

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { DefisPage } from '../pages/defis/defis';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    DefisPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +19,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    DefisPage
   ],
   providers: [
     StatusBar,
